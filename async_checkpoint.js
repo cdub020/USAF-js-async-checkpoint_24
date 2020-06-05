b65f98c0aef1d907fd8697fc25bc353a03d9c075
Chris W <63630268+cdub020@users.noreply.github.com>
2020-06-05 11:47:58 -0700

    Create async_checkpoint.js

---
+++ async_checkpoint.js
@@ -0,0 +1,12 @@
+/*Create a command line application that processes a file list of pokemon names (each name seperated by a new line) 
+  and logs each Pokemon's types (for some there are many) according to the pokeapi.co API.
+
+Example file input (input.txt)
+
+charizard
+pikachu
+Example console output:
+
+Charizard: flying, fire
+Pikachu: electric
+*/
