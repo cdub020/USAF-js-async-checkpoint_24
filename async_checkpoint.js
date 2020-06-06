31bfdf6dd88f65e7624f4c07e1b90408be88d51b
Chris W <63630268+cdub020@users.noreply.github.com>
2020-06-05 21:01:05 -0700

    Update async_checkpoint.js

--- async_checkpoint.js
+++ async_checkpoint.js
@@ -1,40 +1,54 @@
-/*Create a command line application that processes a file list of pokemon names (each name seperated by a new line) 
+/*Create a command line application that processes a file list of pokemon names (each name seperated by a new line)
   and logs each Pokemon's types (for some there are many) according to the pokeapi.co API.
-
 Example file input (input.txt)
-
 charizard
 pikachu
-
 Example console output:
-
 Charizard: flying, fire
 Pikachu: electric
 */
+
 //*** Global Declarations ***
-var fs = require("fs");
+const fs = require("fs");
+const file = "input.txt"
+const pokemon_url = "https://pokeapi.co/api/v2/pokemon/";
+const fetch = require('node-fetch');
 
+read_text = () => {
+  var filearr = [];
+  let textinput = fs.readFileSync(file, "utf8");
 
+  return filearr = textinput.split("\n"); // Output To Array
+}
+
+const getPokemonData = async (pokemon) => {
+  let temparr = [];
+  let pokeobj = {};
+    try{
+      const response = await fetch(pokemon_url + pokemon);
+      const data = await response.json();
+
+      for (var x=0; x < data.types.length; x++){
+          temparr.push(data.types[x].type.name);
+      }
+      pokeobj = {
+        'name' : pokemon,
+        'type' : temparr
+      };
+      return pokeobj;
+    }
+
+  catch{
+    console.log("Oops, you did something wrong.");
+  }
+}
 
 //** MAIN ***
 //read text from file input to variable
-var textinput = fs.readFileSync("input.txt", "utf8");
-var pokemonarr = textinput.split("\n"); // To Array
-var typearr = [];
 
-
-//Using https://pokeapi.co/api/v2/pokemon/ API
-const fetch = require('node-fetch');
-for (var index=0; index < pokemonarr.length;index ++) //loop for all pokemon in input file
-fetch('https://pokeapi.co/api/v2/pokemon/' + pokemonarr[index])
-  .then(response => response.json())
-  .then(json => {
-        for (var x=0; x<json.types.length; x++){ //loop for all types
-            //save each value to a typearray "json.types[x].type.name"
-        }
-        //Save pokeobject {"name" = pokemonarr[index], "Type": typearray
-        // reset typearray value
-  })
-  //for in loop to iterate over object
-  // console.log object properties to seperate lines
-  // Not very good with the syntax yet so taking me long time to look up the different methods
+var pokemonarr = read_text();
+for (var index = 0; index < pokemonarr.length; index++){ 
+    console.log
+    getPokemonData(pokemonarr[index])
+      .then(val => console.log(val.name + ": " + val.type.join(', ')));
+}
